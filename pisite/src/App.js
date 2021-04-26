0eb8ae0d9b974528a1b1a19c575bffa214b2d278
G-5611 <57376461+G-5611@users.noreply.github.com>
2021-04-26 18:44:47 -0300

    New windown and Routing
    +LoginCompany windown
    +Routing links

--- pisite/src/App.js
+++ pisite/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import { Login } from "./Screens/Login"
+import { LoginCompany } from "./Screens/LoginCompany"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/logincompany" component={LoginCompany} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- pisite/src/Screens/LoginCompany/index.js
+++ pisite/src/Screens/LoginCompany/index.js
@@ -2,29 +2,27 @@ import React from "react";
 import "./index.css"
 import { Link } from "react-router-dom"
 
-export const Login = () => {
+export const LoginCompany = () => {
 
   return (
-    <div className="panel-login">
+    <div className="panel-login-empresa">
       <div>
         <img src={"./assets/Logo.png"} alt="logosite" />
       </div>
 
       <form>
         <div className="form-group">
-          <input className="form-control" id="cpf-input" placeholder="CPF" />
+          <input className="form-control" id="cnpj-input" placeholder="CNPJ" />
         </div>
 
         <div className="form-group">
           <input className="form-control" id="passowrd-input" placeholder="Password" />
         </div>
 
-        <Link className="btn btn-link" to="/logincompany">Sou uma empresa</Link>
-
+        <Link className="btn btn-link" to="/">Voltar</Link>
         <br />
 
         <button type="submit" className="btn btn-primary">Login</button>
-
       </form>
     </div>
   )
