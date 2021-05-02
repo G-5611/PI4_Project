54f192ba54bbf2f89d447fe5e7dd5f4f5ffde583
G-5611 <57376461+G-5611@users.noreply.github.com>
2021-05-02 02:22:26 -0300

    Fixing the routing
    Browser Router was giving blank pages when i used build so i changed to HashRouter this fixed the problem

--- pisite/src/App.js
+++ pisite/src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import { Login } from "./Screens/Login"
 import { LoginCompany } from "./Screens/LoginCompany"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { HashRouter, Switch, Route } from "react-router-dom"
 import { StartPage } from './Screens/StartPage';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route exact path="/" component={StartPage} />
           <Route exact path="/loginuser" component={Login} />
           <Route exact path="/logincompany" component={LoginCompany} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
