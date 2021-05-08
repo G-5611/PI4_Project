44c6a3e7b0cbb23840a814d2706003221f1dcbbe
G-5611 <57376461+G-5611@users.noreply.github.com>
2021-05-08 11:12:11 -0300

    Navbar and labels
    Adding navbar to profileuser and changing labels

--- pisite/src/App.js
+++ pisite/src/App.js
@@ -5,7 +5,6 @@ import { HashRouter, Switch, Route } from "react-router-dom"
 import { StartPage } from './Screens/StartPage';
 import { Cadastrocompany } from './Screens/Cadastrocompany';
 import { Cadastrouser } from './Screens/Cadastrouser';
-import { Busca } from './Screens/Busca'; 
 import { Profileuser } from './Screens/Profileuser';
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
           <Route exact path="/logincompany" component={LoginCompany} />
           <Route exact path="/registercompany" component={Cadastrocompany} />
           <Route exact path="/registeruser" component={Cadastrouser} />
-          <Route exact path="/busca" component={Busca} />
           <Route exact path="/profileuser" component={Profileuser} />
         </Switch>
       </HashRouter>
--- pisite/src/Screens/Profileuser/index.js
+++ pisite/src/Screens/Profileuser/index.js
@@ -1,50 +1,61 @@
-import "./index.css"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
+import { Navbar } from "./../../Componentes/Navbar"
+
+import "./index.css"
+
 
 export const Profileuser = () => {
-    return(
-        <div className="panel-profile">
-            <h3>Perfil do Usuário</h3>
-            <br />
-            
-            <form>
-                <img src="./assets/Logo.png" />
-                <div className="form-group">
-                    <input className="form-control" id="nome-completo" placeholder="Nome Completo" />
-                </div> 
-                <div className="form-group">
-                    <select className="form-control" id="uf-select" required>
-                        <option selected disabled value="">UF...</option>
-                        <option>DF</option>
-                    </select>
-                </div>
-                <div className="form-group">
-                    <input className="form-control" id="instituicao" placeholder="Instituição" />
-                </div>
-                <div className="form-group">
-                    <input className="form-control" id="ano-formacao" placeholder="Ano da Formação" />
-                </div> 
-                <div className="form-group">
-                    <select className="form-control" id="curso-select" required>
-                        <option selected disabled value="">Curso...</option>
-                        <option>Computação</option>
-                    </select>
-                </div>
-                <div className="form group">
-                    <input className="form-control" id="biografia-usuario" placeholder="Biografia" />
-                </div>
-                <br />
+  const [Nome, setNome] = useState("Fulano");
 
-                    <Link className="btn btn-link" to="/profileuser">Perfil do Usuário</Link>
+  return (
+    <div >
+      <Navbar />
 
-                <br />
+      <div className="panel-profile">
 
-                    <Link className="btn btn-link" to="/">Voltar</Link>
-            </form>       
+        <h3>Perfil do Usuário</h3>
+
+        <br />
+
+        <img src="./assets/Logo.png" />
+
+        <div className="form-group row">
+          <label className="col-sm-4 col-form-label text-left"><b>Nome Completo:</b></label>
+          <div className="col-sm-7">
+            <div className="form-control-plaintext text-left">{Nome}</div>
+          </div>
         </div>
 
-    )
+        <div className="form-group">
+          <select className="form-control" id="uf-select" required>
+            <option selected disabled value="">UF...</option>
+            <option>DF</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <input className="form-control" id="instituicao" placeholder="Instituição" />
+        </div>
+        <div className="form-group">
+          <input className="form-control" id="ano-formacao" placeholder="Ano da Formação" />
+        </div>
+        <div className="form-group">
+          <select className="form-control" id="curso-select" required>
+            <option selected disabled value="">Curso...</option>
+            <option>Computação</option>
+          </select>
+        </div>
+        <div className="form group">
+          <input className="form-control" id="biografia-usuario" placeholder="Biografia" />
+        </div>
+        <br />
 
+        <Link className="btn btn-link" to="/profileuser">Perfil do Usuário</Link>
 
+        <br />
 
+        <Link className="btn btn-link" to="/">Voltar</Link>
+      </div>
+    </div>
+  )
 }
