6febe7e13cebe93967b35e8ba96abb0b7e2d2680
Mitsune <57376461+G-5611@users.noreply.github.com>
2021-08-19 11:00:45 -0300

    Router Filter by user type

--- pisite/src/App.js
+++ pisite/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import { Login } from "./Screens/Login"
 import { LoginCompany } from "./Screens/LoginCompany"
 import { HashRouter, Switch, Route } from "react-router-dom"
@@ -13,7 +14,16 @@ import { RecuperarSenha } from './Screens/RecoverPassword'
 import { ListaUserVagas } from './Screens/Vacancy/UserVacancyList'
 import { ListaCompanyVagas } from './Screens/Vacancy/CompanyVacancyList'
 
-function App() {
+
+export const App = () => {
+
+  const [Type, setType] = useState("");
+
+  useEffect(() => {
+    const type = localStorage.getItem('type');
+    setType(type)
+  }, [localStorage])
+
   return (
     <div className="App">
       <HashRouter>
@@ -23,19 +33,28 @@ function App() {
           <Route exact path="/logincompany" component={LoginCompany} />
           <Route exact path="/registercompany" component={Cadastrocompany} />
           <Route exact path="/registeruser" component={Cadastrouser} />
-          <Route exact path="/profileuser" component={Profileuser} />
-          <Route path="/profileuser/:id" component={Profileuser} />
-          <Route exact path="/profilecompany" component={Profilecompany} />
-          <Route path="/profilecompany/:id" component={Profilecompany} />
-          <Route exact path="/criarvaga" component={VagaCreate} />
-          <Route path="/criarvaga/:id" component={VagaCreate} />
-          <Route exact path="/vacancy" component={Vacancy} />
-          <Route path="/vacancy/:id/:vacancyId" component={Vacancy} />
           <Route exact path="/recuperarsenha" component={RecuperarSenha} />
-          <Route path="/uservacancylist/:id" component={ListaUserVagas} />
-          <Route exact path="/uservacancylist" component={ListaUserVagas} />
-          <Route path="/companyvacancylist/:id" component={ListaCompanyVagas} />
-          <Route exact path="/companyvacancylist" component={ListaCompanyVagas} />
+
+          {Type !== "company" && (
+            <>
+              <Route exact path="/profilecompany" component={Profilecompany} />
+              <Route path="/profilecompany/:id" component={Profilecompany} />
+              <Route exact path="/criarvaga" component={VagaCreate} />
+              <Route path="/criarvaga/:id" component={VagaCreate} />
+              <Route path="/companyvacancylist/:id" component={ListaCompanyVagas} />
+              <Route exact path="/companyvacancylist" component={ListaCompanyVagas} />
+            </>
+          )}
+          {Type === "user" && (
+            <>
+              <Route exact path="/vacancy" component={Vacancy} />
+              <Route path="/vacancy/:id/:vacancyId" component={Vacancy} />
+              <Route exact path="/uservacancylist" component={ListaUserVagas} />
+              <Route path="/uservacancylist/:id" component={ListaUserVagas} />
+              <Route exact path="/profileuser" component={Profileuser} />
+              <Route path="/profileuser/:id" component={Profileuser} />
+            </>
+          )}
         </Switch>
       </HashRouter>
     </div>
